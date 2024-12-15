d2dc92177d23fe372499f8286ff379117b2c8b49
rajnandinitikkal <rajnandinitikkal@gmail.com>
2024-12-15 13:05:35 +0530

    ...

--- BubbleSort.java
+++ BubbleSort.java
@@ -4,7 +4,7 @@ public class BubbleSort{
         int arr[] = {8, 50, 30,28, 14, 39};
         int length = arr.length;
 
-        for(int i=0, i<length; i++){
+        for(int i=0; i<length; i++){
             for(int j=1; j<length-i; j++){
                 if(arr[j-1] > arr[j]){
                     int temp = arr[j];
@@ -14,8 +14,8 @@ public class BubbleSort{
             }
         }
         System.out.println("Sorted Array: ");
-        for(int i=0, i<length; i++){
-            System.out.print(arr[i] + ",")
+        for(int i=0; i<length; i++){
+            System.out.print(arr[i] + ",");
         }
     }
 }
--- ReverseString.java
+++ ReverseString.java
@@ -2,6 +2,12 @@ public class ReverseString {
     public static void main(String[] args){
 
         String str ="abcd";
-        Str
+        int length = str.length();
+
+        for (int i=length-1; i>=0; i--){
+            
+                System.out.print(str.charAt(i));
+            
+        }
     }
 }
