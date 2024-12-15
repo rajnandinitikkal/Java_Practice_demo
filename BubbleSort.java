4f7098c37bf511645af6772019e2d77a6cb6fb60
rajnandinitikkal <rajnandinitikkal@gmail.com>
2024-12-15 12:55:57 +0530

    .

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,22 +1,21 @@
-
-public class BubbleSort {
+public class BubbleSort{
     public static void main(String[] args){
 
-        int arr[] = {10, 5,9,23,15,20,19};
+        int arr[] = {8, 50, 30,28, 14, 39};
         int length = arr.length;
 
-        for(int i=0; i<length; i++){
+        for(int i=0, i<length; i++){
             for(int j=1; j<length-i; j++){
-                if(arr[j-1]>arr[j]){
-                int temp = arr[j];
-                arr[j] = arr[j-1];
-                arr[j-1] = temp;
+                if(arr[j-1] > arr[j]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
                 }
             }
         }
-        System.out.print("Sorted array: ");
-        for(int i=0; i<length; i++){
-            System.out.print(arr[i] + ", ");
+        System.out.println("Sorted Array: ");
+        for(int i=0, i<length; i++){
+            System.out.print(arr[i] + ",")
         }
     }
 }
--- Practice/BooleanDataTypes.java
+++ Practice/BooleanDataTypes.java
@@ -1,14 +1,14 @@
-class BooleanDataTypes
-{
-  public static void main(String args[]) {
-      boolean var1 = true;
-      if (var1 == true) //checks if the value is true or false
-      {
-          System.out.println("Boolean value is True");
-      }
-      else
-      {
-          System.out.println("Boolean value is False");  
-      }
-  }
-}
+// class BooleanDataTypes
+// {
+//   public static void main(String args[]) {
+//       boolean var1 = true;
+//       if (var1 == true) //checks if the value is true or false
+//       {
+//           System.out.println("Boolean value is True");
+//       }
+//       else
+//       {
+//           System.out.println("Boolean value is False");  
+//       }
+//   }
+// }
