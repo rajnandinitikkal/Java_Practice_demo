f75c07e0ff6618ec0b6309f01ea1c0ed5c51fef7
rajnandinitikkal <rajnandinitikkal@gmail.com>
2024-12-18 21:33:08 +0530

    Enum

---
+++ Enum/Learning.java
@@ -0,0 +1,13 @@
+public enum Learning {
+    COREJAVA(10) ,COLLECTIONS(20) ,GENERIC(30) ,JSP(7) ,MULTITHREADING(10);
+
+    private int i;
+  
+    Learning(int i){
+      this.i =i;
+  }
+  
+    public int getI() {
+      return i;
+    }
+}
---
+++ Enum/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        
+               Learning learning = Learning.MULTITHREADING ;
+
+               System.out.println(learning);
+
+               System.out.println(Learning.COLLECTIONS);
+               System.out.println(Learning.JSP.getI());
+        
+        
+        //        switch (learning){
+        //            case COREJAVA -> System.out.println("coe java");
+        //            case COLLECTIONS -> System.out.println("collection");
+        //            case GENERIC -> System.out.println("generic");
+        //            case JSP -> System.out.println("JSP");
+        //            case MULTITHREADING -> System.out.println("multi threading");
+        //        }
+        
+                
+            }
+        }
