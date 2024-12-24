714fbfac0c42d0828a5a4c6a3638b6d79a700ba2
rajnandinitikkal <rajnandinitikkal@gmail.com>
2024-12-24 15:39:14 +0530

    add interview question

---
+++ Intervie_Prep/Main.java
@@ -0,0 +1,23 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        int start = 2;
+        int end = 8;
+        for(int i=start; i<=end; i++) {
+            if(i % 3 == 0 && i % 7 ==0){
+                System.out.println("CLOUDNIX");
+            }
+            else if(i % 3 == 0 ){
+                System.out.println("CLOUD");
+            }
+            else if(i % 7 == 0){
+                System.out.println("NIX");
+            }
+            else{
+                System.out.println(i);
+            }
+
+}
+}
+}
