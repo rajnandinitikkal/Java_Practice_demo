2c13ddaae259dc2b1fc392c2f997af46024edeb6
rajnandinitikkal <rajnandinitikkal@gmail.com>
2025-01-01 16:30:27 +0530

    adding generics

---
+++ Generic/Main.java
@@ -0,0 +1,87 @@
+
+import java.util.LinkedList;
+import java.util.List;
+
+// class Data{
+//    private Object object;
+
+//    public Data(Object object) {
+//        this.object = object;
+//    }
+//    @Override
+//    public String toString() {
+//        return object.toString();
+//    }
+//    public Object getObject() {
+//        return object;
+//    }
+// }
+
+class Data<T>{
+    private T object;
+
+    public Data(T object) {
+        this.object = object;
+    }
+    @Override
+    public String toString() {
+        return object.toString();
+    }
+   public Object getObject() {
+       return object;
+   }
+}
+
+class Name{
+   private String name;
+
+   public Name(String name) {
+       this.name = name;
+   }
+
+   @Override
+   public String toString() {
+       return name;
+   }
+}
+
+class GenericData<T>{
+   private T somedata;
+
+   public GenericData(T somedata) {
+       this.somedata = somedata;
+   }
+
+   public T getSomedata() {
+       return somedata;
+   }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Data<Object>> elements = new LinkedList<>();
+        elements.add(new Data<>("Tuhsar"));
+       elements.add(new Data<>(54));
+       elements.add(new Data<>(78.9));
+        elements.add(new Data<>("Rutuja"));
+
+    //     for(Data data:elements){
+    //         System.out.println(data);
+    //     }
+    //    System.out.println(elements);
+
+//        *********** using simple method ******************
+
+    //    Data data = new Data("Some data");
+    //    String x = (String) data.toString();
+    //    System.out.println(x);
+
+
+//        **************** Using Generic Type **************
+       GenericData<String> gendata = new GenericData<>("some generic data");
+       String y = gendata.getSomedata();
+       System.out.println(y);
+    }
+}
---
+++ Generic/gen.java
@@ -0,0 +1,45 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Data{
+    public <E> void printListData(List<E> list){
+        for (E elements : list) {
+            System.out.println(elements);
+        }
+    }
+    public <E> void printArrayData(E[] array){
+        for(E elements : array){
+            System.out.println(elements);
+        }
+    }
+}
+
+public class gen {
+    public static void main(String[] args) {
+
+       List<Integer> list1 = new ArrayList<>();
+       list1.add(12);
+       list1.add(16);
+       list1.add(30);
+       list1.add(20);
+       list1.add(10);
+       System.out.println(list1);
+       new Data().printListData(list1);
+
+       List<String> list2 = new ArrayList<>();
+       list2.add("Tushar");
+       list2.add("Rutuja");
+       list2.add("Nisha");
+       list2.add("Sai");
+       list2.add("Pallavi");
+
+       new Data().printListData(list2);
+
+        String[] strdata = {"Tushar","Navin","Shreya"};
+        new Data().printArrayData(strdata);
+
+        Integer[] intarray = {1,2,3};
+        new Data().printArrayData(intarray);
+    }
+}
