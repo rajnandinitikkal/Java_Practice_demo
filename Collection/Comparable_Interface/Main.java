f3c9369ebc8ae79ddc54e341a2d3260db925d12b
rajnandinitikkal <rajnandinitikkal@gmail.com>
2025-01-03 16:13:22 +0530

    add linkedlist

--- Collection/Comparable_Interface/Main.java
+++ Collection/Comparable_Interface/Main.java
@@ -1,5 +1,55 @@
 package Collection.Comparable_Interface;
 
+import java.util.LinkedList;
+import java.util.List;
+
+class Name implements Comparable<Name>{
+    private String name;
+
+    public Name(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public int compareTo(Name obj) {
+        if(name.length() == obj.name.length()){
+            return 0;
+        }
+        else if(name.length() > obj.name.length()){
+            return -1;
+        }
+        else{
+            return 1;
+        }
+
+    }
+}
+
 public class Main {
-    
+    public static void main(String[] args) {
+
+//        String x = "b";
+//        String y = "t";
+//
+//        System.out.println(x.compareTo(y));
+
+        List<Name> elements = new LinkedList<>();
+        elements.add(new Name("Rutuja"));
+        elements.add(new Name("Raj"));
+        elements.add(new Name("Ritha"));
+        elements.add(new Name("Pooja"));
+        elements.add(new Name("Priya"));
+
+        System.out.println(elements);
+
+        elements.sort(null);
+        System.out.println(elements);
+
+
+    }
 }
--- Collection/LinkedList/Main.java
+++ Collection/LinkedList/Main.java
@@ -1,2 +1,46 @@
 package Collection.LinkedList;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        LinkedList<String> names = new LinkedList<>();
+//        names.add("Rutuja");
+//        names.add(2, "Tushar");
+//        names.add("Prajkta");
+//        names.add("Vishal");
+//
+//        System.out.println(names);
+//
+//        names.add("Ritha");
+//        names.add(1, "Pallavi");
+//        System.out.println(names);
+
+        LinkedList<String> list1 = new LinkedList<>();
+        list1.add("Riman");
+        list1.add("AAyan");
+        list1.add("Radha");
+//        System.out.println(list1);
+
+        list1.add(3,"Pavan");
+        System.out.println(list1);
+
+        ArrayList<String> list2 = new ArrayList<>();
+        list2.add("Krishna");
+        list2.add("Balveer");
+        list2.add("Pankaj");
+//        System.out.println(list2);
+
+        list2.sort(null);
+        System.out.println(list2);
+
+        Collections.reverse(list2);
+        System.out.println(list2);
+
+list2.add(3,"Shardhha");
+//        System.out.println(list2);
+    }
+}
